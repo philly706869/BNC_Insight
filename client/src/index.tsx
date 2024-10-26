17377a490c783516f58c3415d6f16dc44cb4a712
PhillY <philly706869@gmail.com>
2024-10-26 16:10:40 +0900

    commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { getCategories } from "./services/category-service";
 
 // body에 div 추가 후 div에 <App /> 렌더
 const rootElement = document.createElement("div");
 document.body.appendChild(rootElement);
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
-
-getCategories().then(console.log);
-fetch("/api/categories")
-  .then((data) => data.text())
-  .then(console.log);
