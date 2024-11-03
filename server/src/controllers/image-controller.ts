abac2ef250109dd80b5729805ee376beb0005b74
PhillY <philly706869@gmail.com>
2024-11-03 22:22:32 +0900

    commit

--- server/src/controllers/image-controller.ts
+++ server/src/controllers/image-controller.ts
@@ -1,9 +1,11 @@
+import { config } from "@/config";
 import { env } from "@/env";
 import { ImageNotFoundError } from "@/errors/service-errors";
 import { ImageService } from "@/services/image-service";
 import { authorize } from "@/utils/authorize";
 import { NextFunction, Request, Response } from "express";
 import fs from "fs/promises";
+import multer, { MulterError } from "multer";
 import path from "path";
 import { z } from "zod";
 
@@ -40,17 +42,58 @@ export class ImageController {
     }
   }
 
+  private static readonly upload = multer({
+    dest: path.resolve(config.image.tempPath),
+    limits: { fileSize: config.image.maxBytes },
+    fileFilter(req, file, callback) {
+      for (const format of config.image.supportedFormats) {
+        if (file.mimetype.endsWith(format)) {
+          callback(null, true);
+          return;
+        }
+      }
+      callback(null, false);
+    },
+  }).single("image");
+
   public async post(req: Request, res: Response): Promise<void> {
     const userUid = authorize(req, res);
     if (userUid === undefined) {
       return;
     }
 
-    const file = req.file;
-    if (file === undefined) {
-      res.status(400).end();
-      return;
+    try {
+      await new Promise<void>((resolve, reject) => {
+        ImageController.upload(req, res, (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+    } catch (error) {
+      if (!(error instanceof MulterError)) {
+        return Promise.reject(error);
+      }
+      if (error.code === "LIMIT_FILE_SIZE") {
+        res.status(413).error({
+          error: "FILE_TOO_LARGE",
+          message: `File cannot bigger than ${config.image.maxBytes} bytes`,
+        });
+        return;
+      }
+      if (error.code === "LIMIT_UNEXPECTED_FILE") {
+        res.status(415).error({
+          error: "UNSUPPORTED_FILE",
+          message: "Unsupported file format",
+        });
+        return;
+      }
+      return Promise.reject(error);
     }
+
+    const file = req.file!;
 
     const imagePath = path.resolve(file.destination, file.filename);
     try {
--- server/src/routers/image-router.ts
+++ server/src/routers/image-router.ts
@@ -1,10 +1,7 @@
-import { config } from "@/config";
 import { ImageController } from "@/controllers/image-controller";
 import { ImageService } from "@/services/image-service";
 import { Router } from "express";
 import { rateLimit } from "express-rate-limit";
-import multer, { MulterError } from "multer";
-import path from "path";
 
 export const imageRouter = Router();
 const service = new ImageService();
@@ -13,22 +10,6 @@ const controller = new ImageController(service);
 imageRouter.get("/:name", (req, res, next) =>
   controller.get(req, res, next).catch(next)
 );
-
-const supportedMIMETypes = config.image.supportedFormats.map(
-  (format) => `image/${format}`
-);
-
-const upload = multer({
-  dest: path.resolve(config.image.tempPath),
-  limits: { fileSize: config.image.maxBytes },
-  fileFilter(req, file, callback) {
-    if (!supportedMIMETypes.includes(file.mimetype)) {
-      callback(null, false);
-      return;
-    }
-    callback(null, true);
-  },
-}).single("image");
 
 const rateLimiter = rateLimit({
   windowMs: 60000,
@@ -41,31 +22,6 @@ const rateLimiter = rateLimit({
   },
 });
 
-imageRouter.post(
-  "/",
-  rateLimiter,
-  (req, res, next) => {
-    upload(req, res, (error) => {
-      if (!(error instanceof MulterError)) {
-        next(error);
-        return;
-      }
-      if (error.code === "LIMIT_FILE_SIZE") {
-        res.status(413).error({
-          error: "FILE_TOO_LARGE",
-          message: `File cannot bigger than ${config.image.maxBytes} bytes`,
-        });
-        return;
-      }
-      if (error.code === "LIMIT_UNEXPECTED_FILE") {
-        res.status(415).error({
-          error: "UNSUPPORTED_FILE",
-          message: "Unsupported file format",
-        });
-        return;
-      }
-      next(error);
-    });
-  },
-  (req, res, next) => controller.post(req, res).catch(next)
+imageRouter.post("/", rateLimiter, (req, res, next) =>
+  controller.post(req, res).catch(next)
 );
